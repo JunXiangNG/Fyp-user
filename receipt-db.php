19f80bbc8db35daa9197f37c060f9edb0ef51efa
JunXiangNG <127810577+JunXiangNG@users.noreply.github.com>
2023-06-11 22:48:50 +0800

    Update receipt-db.php

--- receipt-db.php
+++ receipt-db.php
@@ -8,13 +8,26 @@ if (isset($_SESSION['username'])) {
 
     if (isset($_GET['order_id'])) {
         $order_id = $_GET['order_id'];
-
-        $select_query = "SELECT orders.*, orders.order_status, orders.product_price, orders.user_quantity,checkout.order_timestamp, checkout.received_name, checkout.address, checkout.phone ,checkout.town_city,checkout.zip_postalcode,checkout.state_province
+        $select_query = "SELECT orders.order_id, orders.order_timestamp,order_details.user_quantity ,receipts.received_name, receipts.address, receipts.town_city, receipts.state_province, receipts.zip_postalcode, receipts.phone
         FROM orders
-        INNER JOIN checkout ON orders.order_id = checkout.order_id 
-        WHERE orders.order_id = '$order_id' AND orders.username = '$username'";
+        INNER JOIN order_details ON orders.order_id = order_details.order_id 
+        INNER JOIN receipts ON orders.order_id = receipts.order_id
+        WHERE orders.username = '$username' AND orders.order_id = '$order_id'";
+    $result = mysqli_query($connect, $select_query);
 
-        $result = mysqli_query($connect, $select_query);
+    if ($result) {
+        if (mysqli_num_rows($result) > 0) {
+            // Existing code here
+        } else {
+            echo "No rows found.";
+        }
+    } else {
+        echo "Query Error: " . mysqli_error($connect);
+    }
+    
+      
+        
+        
 
         if ($result && mysqli_num_rows($result) > 0) {
             $invoice = mysqli_fetch_assoc($result);
@@ -70,22 +83,39 @@ if (isset($_SESSION['username'])) {
             $pdf->SetFont('Arial', '', 12);
             // Fetch and display items
 
-            $select_query_items = "SELECT orders.*, orders.order_status, orders.product_price, orders.user_quantity, checkout.received_name, checkout.address, checkout.phone
+            $select_query_items = "SELECT DISTINCT orders.order_id, orders.order_status,product.product_name, product_details.product_price, order_details.user_quantity, receipts.received_name, receipts.address, receipts.phone
             FROM orders
-            INNER JOIN checkout ON orders.order_id = checkout.order_id 
+            INNER JOIN order_details ON orders.order_id = order_details.order_id 
+            INNER JOIN receipts ON orders.order_id = receipts.order_id 
+            INNER JOIN product ON order_details.product_id = product.product_id
+            INNER JOIN product_details ON order_details.product_id = product_details.product_id
             WHERE orders.order_id = '$order_id' AND orders.username = '$username'";
+        
         
             $result_items = mysqli_query($connect, $select_query_items);
             
             $subtotal = 0;
             while ($item = mysqli_fetch_assoc($result_items)) {
-                $pdf->Cell(130          , 5,  $item['product_name'], 1,0);
-                $pdf->Cell(25           , 5, $item['user_quantity'], 1, 0);
+                $pdf->Cell(130, 5, $item['product_name'], 1, 0);
+                $pdf->Cell(25, 5, $item['user_quantity'], 1, 0);
                 $pdf->Cell(34, 5, 'RM ' . number_format($item['product_price'] * $item['user_quantity']), 1, 1, 'R');
-
+                
+            
                 $subtotal += $item['product_price'] * $item['user_quantity'];
             }
+            
         
+$result_items = mysqli_query($connect, $select_query_items);
+
+if ($result_items) {
+    if (mysqli_num_rows($result_items) > 0) {
+        // Existing code here
+    } else {
+        echo "No rows found.";
+    }
+} else {
+    echo "Query Error: " . mysqli_error($connect);
+}
 
             // Add summary
             $pdf->Cell(130	,5,'',0,0);
